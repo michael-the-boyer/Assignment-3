e1e447f172e9fbb82e1fcec4b58a19434507bc38
michaeltheboyer <michael.boyer@ocsbstudent.ca>
2021-02-14 20:28:22 +0000

    Switched code order.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("display-alert").addEventListener("click",buttonFunction);
 
 function buttonFunction() {
-  alert("You pressed the button! You should now go get yourself a soda, man. You deserve it.")
   document.body.style.background = "green";
+  alert("You pressed the button! You should now go get yourself a soda, man. You deserve it.")
 }
